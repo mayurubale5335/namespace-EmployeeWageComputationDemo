66a31f5cd86756e63f1e2f7c7836b0ce68397286
unknown <mayurubale5335@gmail.com>
2023-04-07 14:51:15 +0530

    [mayur ubale] Modify finding employee attendance

--- Program.cs
+++ Program.cs
@@ -10,9 +10,21 @@ namespace EmployeeWageComputationDemo
     {
         static void Main(string[] args)
         {
+            int isPresent = 1;
+            // welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
+            // UC1-EmployeeAttendance
+            Random random = new Random();
+            int employeeAttendance = random.Next(0, 2); //0 or 1
+            if(employeeAttendance==isPresent)
+            {
+                Console.WriteLine("employee Attendance is present");
+            }
+            else
+            {
+                Console.WriteLine("employee Attendance is absent");
+            }
             Console.ReadLine();
-
         }
     }
 }

2eeaa6e6347ba603210778e3451ead0edd1b5bfb
unknown <mayurubale5335@gmail.com>
2023-04-09 11:48:17 +0530

    [mayur ubale] Modify adding part time

--- Program.cs
+++ Program.cs
@@ -11,24 +11,31 @@ namespace EmployeeWageComputationDemo
         static void Main(string[] args)
         {
             int empHrs = 0;
-            int isPresent = 1;
+            const int IS_FULL_TIME = 1;
+            const int IS_PART_TIME = 2;
             int empWage = 0;
             const int EMP_RATE_PER_HR = 20;
             // welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             // UC1-EmployeeAttendance
             Random random = new Random();
-            int employeeAttendance = random.Next(0, 2); //0 or 1
-            if(employeeAttendance==isPresent)
+            int employeeAttendance = random.Next(0, 3); //0 or 1
+            if (employeeAttendance == IS_FULL_TIME)
             {
-                Console.WriteLine("employee Attendance is present");
+                Console.WriteLine(" Fulltime employee Attendance is present");
                 empHrs = 8;
-            } 
+            }
+            else if (employeeAttendance == IS_PART_TIME)
+            {
+                Console.WriteLine("Parttime employee Attendance is present");
+                empHrs = 4;
+            }
             else
             {
                 Console.WriteLine("employee Attendance is absent");
-              //  empHrs = 0;
+                //  empHrs = 0;
             }
+            //UC2
             empWage = EMP_RATE_PER_HR * empHrs;
             Console.WriteLine("Employee Wage is" + empWage);
             Console.ReadLine();

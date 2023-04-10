231f927571bba63733348fcb29efbdd18e1d2233
unknown <mayurubale5335@gmail.com>
2023-04-10 18:07:48 +0530

    [mayur ubale] Modify checking total working hrs days

--- Program.cs
+++ Program.cs
@@ -15,17 +15,19 @@ namespace EmployeeWageComputationDemo
             int empHrs = 0;
             int empWage = 0;
             int totalWage = 0;
-            int day = 0;
+            int day = 1;
+            int totalHrs = 0;
             const int IS_FULL_TIME = 1;
             const int IS_PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int NUMBER_OF_WORKING_DAYS = 20;
+            const int MAX_WORKING_HRS = 100;
 
             // welcome message
             Console.WriteLine("Welcome to Employee Wage Computation Problem Statement");
             // UC1-EmployeeAttendance
             Random random = new Random();
-            for (day = 1; day <= NUMBER_OF_WORKING_DAYS; day++)
+           while(day<=NUMBER_OF_WORKING_DAYS && totalHrs<=MAX_WORKING_HRS)
             {
                 int employeeAttendance = random.Next(0, 3); //0 or 1
                 switch (employeeAttendance)
@@ -46,11 +48,13 @@ namespace EmployeeWageComputationDemo
 
                 //UC2
                 empWage = EMP_RATE_PER_HR * empHrs;
-                Console.WriteLine("Employee Wage for day{0} is:{1}", day, empWage);
+                Console.WriteLine("Employee Wage for day{0} and {1}hrs is:{2}", day,empHrs, empWage);
                 totalWage = totalWage + empWage;
                 // totalWage += empWage;
+                day++;
+                totalHrs += empHrs;
             }
-            Console.WriteLine("TotalWage for {0} days is:{1}", (day - 1), totalWage);
+            Console.WriteLine("TotalWage for {0} days and {1} hrs is:{2}", (day - 1),totalHrs, totalWage);
             Console.ReadLine();
         }
     }
